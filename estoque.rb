8314338b1c83ada71775de557231e648eb25afea
zabotg <zabot.gui@gmail.com>
2017-04-11 17:50:14 -0300

    Mudando para method_missing

--- estoque.rb
+++ estoque.rb
@@ -10,23 +10,18 @@ class Estoque
       @livros.extend Contador
    end
 
-   def quantidade_de_vendas_por(produto, &campo)
-      @vendas.count {|venda| campo.call(venda) == campo.call(produto)}
+   def method_missing(nome)
+      matcher = nome.to_s.match "(.+)_que_mais_vendeu_por_(.+)"
+      if matcher
+         tipo = matcher[1]
+         campo = matcher[2].to_sym
+         que_mais_vendeu_por(tipo, &campo)
+      else
+         super
+      end
    end
-
-   def livro_que_mais_vendeu_por(&campo)
-      que_mais_vendeu_por("livro", &campo)
-   end
-
-   def revista_que_mais_vendeu_por(&campo)
-      que_mais_vendeu_por("revista", &campo)
-   end
-
-   def que_mais_vendeu_por(tipo, &campo)
-      @vendas.select {|l| l.tipo == tipo}.sort {|v1,v2|
-         quantidade_de_vendas_por(v1, &campo) <=>
-         quantidade_de_vendas_por(v2, &campo)
-      }.last
+   def respond_to?(nome)
+      nome.to_s.match("(.+)_que_mais_vendeu_por_(.+)") || super
    end
 
    def exporta_csv
@@ -57,5 +52,17 @@ class Estoque
 
    def maximo_necessario
       @livros.maximo_necessario
+   end
+
+   private
+   def quantidade_de_vendas_por(produto, &campo)
+      @vendas.count {|venda| campo.call(venda) == campo.call(produto)}
+   end
+
+   def que_mais_vendeu_por(tipo, &campo)
+      @vendas.select {|l| l.tipo == tipo}.sort {|v1,v2|
+         quantidade_de_vendas_por(v1, &campo) <=>
+         quantidade_de_vendas_por(v2, &campo)
+      }.last
    end
 end
--- sistema.rb
+++ sistema.rb
@@ -15,13 +15,17 @@ arquitetura = Livro.new("Introducao a Arquitetura e Design de Software", 70, 201
 programmer = Livro.new("The Pragmatic Programmer", 100, 1999, true, "", "livro")
 ruby = Livro.new("Programming Ruby", 100, 2004, true, "", "livro")
 revistona = Livro.new("Revista de Ruby", 10, 2012, true, "Revista", "revista")
+online_arquitetura = Livro.new("Introducao a Arquitetura e Design de Software", 50, 2012, true, "", "ebook")
 
 estoque = Estoque.new
-estoque << algoritmos << algoritmos << ruby << programmer << arquitetura << ruby << ruby << revistona << revistona
-
+estoque << algoritmos << algoritmos << ruby << programmer << arquitetura <<
+ruby << ruby << revistona << revistona << online_arquitetura
 estoque.vende ruby
 estoque.vende algoritmos
 estoque.vende algoritmos
 estoque.vende revistona
-puts estoque.livro_que_mais_vendeu_por(&:titulo).titulo
-puts estoque.revista_que_mais_vendeu_por(&:titulo).titulo
+estoque.vende online_arquitetura
+puts estoque.livro_que_mais_vendeu_por_titulo.titulo
+puts estoque.revista_que_mais_vendeu_por_titulo.titulo
+puts estoque.ebook_que_mais_vendeu_por_titulo.titulo
+puts estoque.respond_to?(:ebook_que_mais_vendeu_por_titulo)
